0cf3cc6bc3a8dc5d49b7405c46327bf3ae7b606a
yestin <yestiny@outlook.com>
2026-04-11 01:05:50 +0800

    实现大模型对话，apikey从环境变量中获取

---
+++ agent/agent.py
@@ -0,0 +1,37 @@
+from langchain_openai import ChatOpenAI
+from config.config import get_config
+
+def main():
+
+    api_config = get_config()
+
+    print(api_config["api_key"][:5] + "*****")
+
+    """参考百炼文档实现https://bailian.console.aliyun.com/cn-beijing/?spm=5176.12818093_47.overview_recent.1.7fa82cc9Fv7yKk&tab=api#/api/?type=model&url=2833609"""
+
+    llm = ChatOpenAI(
+        api_key=api_config["api_key"],
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+        model="qwen-plus-2025-07-14",
+        
+    )
+
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "你是谁？"}]
+
+
+    """一次性输出res = llm.invoke(messages)
+    print(res.model_dump_json())"""
+
+
+    """流式输出"""
+
+    res = llm.stream(messages)
+
+    for chunk in res:
+        print(chunk.model_dump_json())
+
+if __name__ == "__main__":
+    main()
+
---
+++ config/config.py
@@ -0,0 +1,16 @@
+import os
+
+print("加载的顶层代码")
+
+def get_config():
+
+    return {
+        "api_key": os.getenv("ANTHROPIC_API_KEY"),
+        "base_url": os.getenv("ANTHROPIC_BASE_URL"),
+        "model": os.getenv("ANTHROPIC_MODEL"),
+    }
+
+if __name__ == "__main__":
+    print("用main控制的加载的顶层代码")
+    config = get_config()
+    print(config)
